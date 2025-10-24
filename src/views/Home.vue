4f243848793fbae24d38576d6f2f63f138c051fe
Fabián Torres <fabianpenatorres88@gmail.com>
2025-10-24 18:04:03 -0300

    fix import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 import { ref, computed, onMounted, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuth } from '@/composables/useAuth'
-import { getPokemon } from '@/services/pokeApi.js'
+import { getPokemon } from '@/services/pokeapi.js'
 
 const router = useRouter()
 const { user, logout } = useAuth()
